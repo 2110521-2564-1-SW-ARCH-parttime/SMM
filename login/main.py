84a40994160e1a1a9b3d38af0cad4ceedba50e5c
wichukornk <64311553+wichukornk@users.noreply.github.com>
2021-10-03 20:00:52 +0700

    create login page
    First commit of login page. Revise from example and use local database

--- login/main.py
+++ login/main.py
@@ -1,105 +1,67 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-
-
+from flask import Flask,render_template,request,redirect
+from flask_login import login_required, current_user, login_user, logout_user
+from models import UserModel,db,login
+ 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.secret_key = 'xyz'
+ 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-##CREATE TABLE
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
-# db.create_all()
-
-
-@app.route('/')
-def home():
-    return render_template("index.html", logged_in=current_user.is_authenticated)
-
-
-@app.route('/register', methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-
-        if User.query.filter_by(email=request.form.get('email')).first():
-            #User already exists
-            flash("You've already signed up with that email, log in instead!")
-            return redirect(url_for('login'))
-
-        hash_and_salted_password = generate_password_hash(
-            request.form.get('password'),
-            method='pbkdf2:sha256',
-            salt_length=8
-        )
-        new_user = User(
-            email=request.form.get('email'),
-            name=request.form.get('name'),
-            password=hash_and_salted_password,
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user)
-        return redirect(url_for("secrets"))
-
-    return render_template("register.html", logged_in=current_user.is_authenticated)
-
-
-@app.route('/login', methods=["GET", "POST"])
+ 
+ 
+db.init_app(app)
+login.init_app(app)
+login.login_view = 'login'
+ 
+@app.before_first_request
+def create_all():
+    db.create_all()
+     
+@app.route('/blogs')
+@login_required
+def blog():
+    return render_template('blog.html')
+ 
+ 
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
-    if request.method == "POST":
-        email = request.form.get('email')
-        password = request.form.get('password')
-    
-        user = User.query.filter_by(email=email).first()
-        #Email doesn't exist or password incorrect.
-        if not user:
-            flash("That email does not exist, please try again.")
-            return redirect(url_for('login'))
-        elif not check_password_hash(user.password, password):
-            flash('Password incorrect, please try again.')
-            return redirect(url_for('login'))
-        else:
+    if current_user.is_authenticated:
+        return redirect('/blogs')
+     
+    if request.method == 'POST':
+        email = request.form['email']
+        user = UserModel.query.filter_by(email = email).first()
+        if user is not None and user.check_password(request.form['password']):
             login_user(user)
-            return redirect(url_for('secrets'))
-
-    return render_template("login.html", logged_in=current_user.is_authenticated)
-
-
-@app.route('/secrets')
-@login_required
-def secrets():
-    print(current_user.name)
-    return render_template("secrets.html", name=current_user.name, logged_in=True)
-
-
+            return redirect('/blogs')
+     
+    return render_template('login.html')
+ 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect('/blogs')
+     
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+ 
+        if UserModel.query.filter_by(email=email).first():
+            return ('Email already Present')
+             
+        user = UserModel(email=email, username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('register.html')
+ 
+ 
 @app.route('/logout')
-@login_required
 def logout():
     logout_user()
-    return redirect(url_for('home'))
-
-
-@app.route('/download')
-@login_required
-def download():
-    return send_from_directory('static', filename="files/cheat_sheet.pdf")
-
+    return redirect('/blogs')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='localhost', port=5000)
