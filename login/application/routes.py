70262c192351f8562c074ecee3b413171b74115a
wichukornk <64311553+wichukornk@users.noreply.github.com>
2021-10-22 20:36:28 +0700

    Connect app to mongo (local)

--- login/application/routes.py
+++ login/application/routes.py
@@ -1,3 +1,5 @@
+from os import name
+from flask.helpers import url_for
 from flask.wrappers import Response
 from application import app,db
 from flask import json, render_template, request, json, Response, flash, redirect
@@ -14,17 +16,43 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if request.form.get("email") == "test@technos.com":
-            flash("You are successfully logged in!", "success")
-            # return redirect("/index")
-            return render_template("index.html", login=True)
+        email = form.email.data
+        password = form.password.data
+
+        user = User.objects(email=email).first()
+        
+        # print(user.password) # k9doaly
+
+        if user and user.get_password(password):
+            flash(f"{user.first_name}, You are successfully logged in!", "success")
+            return render_template("index.html", login=True, name=user.first_name)
         else:
             flash("Sorry, something went wrong.","danger")
-    return render_template("login.html", title="Login", form=form, login=True)
+    return render_template("login.html", title="Login", form=form, login=True )
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST'])
 def register():
-    return render_template("register.html", register=True)
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_id = User.objects.count()
+        user_id += 1
+
+        email = form.email.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        print(f"{user_id} {email} {password} {first_name} {last_name}")
+
+        user = User(user_id=user_id, email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+
+        flash("You are successfully registered!", "success")
+
+        return render_template("index.html", login=True, name=user.first_name)
+
+    return render_template("register.html", title="Register", form=form, register=True)
 
 
--- login/config.py
+++ login/config.py
@@ -4,6 +4,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
 
     MONGODB_SETTINGS = { 'db' : 'SMM',
-                        # 'host' : 'mongodb://localhost:27017/UTA_Enrollment'
+                        'host' : 'mongodb://localhost:27017/SMM'
                         }
     
