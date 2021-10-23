dbe91a9096bab091dbd8ae28cbf7edbb277bc7ed
wichukornk <64311553+wichukornk@users.noreply.github.com>
2021-10-23 20:02:33 +0700

    Collect user login in session

--- login/application/routes.py
+++ login/application/routes.py
@@ -2,7 +2,7 @@ from os import name
 from flask.helpers import url_for
 from flask.wrappers import Response
 from application import app,db
-from flask import json, render_template, request, json, Response, flash, redirect
+from flask import json, render_template, request, json, Response, flash, redirect, session
 from application.models import User
 from application.forms import LoginForm, RegisterForm
 
@@ -10,22 +10,34 @@ from application.forms import LoginForm, RegisterForm
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html",index=True)
+    login = False
+    first_name = ""
+    if session.get('username'):
+        login = True
+        first_name = session.get('username')
+    
+    print(login)
+
+    return render_template("index.html", login=login, name=first_name)
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+
+    if session.get('username'):
+        return redirect(url_for('index'))
+    
     form = LoginForm()
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
 
         user = User.objects(email=email).first()
-        
-        # print(user.password) # k9doaly
 
         if user and user.get_password(password):
             flash(f"{user.first_name}, You are successfully logged in!", "success")
-            return render_template("index.html", login=True, name=user.first_name)
+            session['user_id'] = user.user_id
+            session['username'] = user.first_name
+            return redirect(url_for('index'))
         else:
             flash("Sorry, something went wrong.","danger")
     return render_template("login.html", title="Login", form=form, login=True )
@@ -33,6 +45,8 @@ def login():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
+    if session.get('username'):
+        return redirect(url_for('/index'))
     form = RegisterForm()
     if form.validate_on_submit():
         user_id = User.objects.count()
@@ -51,7 +65,7 @@ def register():
 
         flash("You are successfully registered!", "success")
 
-        return render_template("index.html", login=True, name=user.first_name)
+        return redirect(url_for('/index'))
 
     return render_template("register.html", title="Register", form=form, register=True)
 
