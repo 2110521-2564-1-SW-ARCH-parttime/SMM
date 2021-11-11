6d5b04d734f4f165e0ae9648acd6331efaad3409
wichukornk <64311553+wichukornk@users.noreply.github.com>
2021-11-11 13:21:27 +0700

    Release 2.0
    Major
    - Change UI design
    - Add delete cart service
    Minor
    - Improve add product service

--- show_catalog/main.py
+++ show_catalog/main.py
@@ -23,11 +23,11 @@ def catalog(shop):
     session['shop'] = shop
     output = []
     i = 0
-    print(query[0])
-    # for x in query:
-    #     output.append(x)
-    #     i += 1
-    # # print(output)
+    # print(query[0])
+    for x in query:
+        output.append(x)
+        i += 1
+    # print(output)
     return render_template("catalog.html", productData=output, shop=shop)
 
 @app.route("/cart", methods=["GET","POST"])
@@ -37,18 +37,18 @@ def cart():
     prodAmount = int(request.form.get('amount'))
     prodPrice = int(request.form.get('price'))
 
-    if 'cart_id' not in session:
-        session['cart_id'] = request.remote_addr+"_"+session.get('shop')
-    
-    cart_id = session.get('cart_id')
-
     if 'cart_list' in session:
-        session['cart_list'] += [{'id':prodID,'product':prodName ,'amount':prodAmount,'cost':prodPrice*prodAmount}]
+        prodIds = [prod.get('id') for prod in session['cart_list']]
+        if prodID in prodIds:
+            # print(session['cart_list'][prodIds.index(prodID)])
+            session['cart_list'][prodIds.index(prodID)]['amount'] += prodAmount
+            # print(prodIds.index(prodID))
+        else:
+            session['cart_list'] += [{'id':prodID,'product':prodName ,'amount':prodAmount,'price':prodPrice}]
     else:
-        session['cart_list'] = [{'id':prodID,'product':prodName ,'amount':prodAmount,'cost':prodPrice*prodAmount}]
-
+        session['cart_list'] = [{'id':prodID,'product':prodName ,'amount':prodAmount,'price':prodPrice}]
     # flash(f'You added Product ID:{prodID} Product Name:{prodName} Amount:{prodAmount} to cart {cart_id}')
-    flash(session['cart_list'])
+    # flash(session['cart_list'])
 
     return redirect('catalog/'+session.get('shop'))
 
@@ -56,46 +56,36 @@ def cart():
 def order():
     return render_template("order.html")
 
+@app.route("/cancel")
+def cancel():
+    if 'cart_list' in session:
+        session.pop('cart_list')
+    return redirect('catalog/'+session.get('shop'))
+
 @app.route("/sendOrder", methods=["GET","POST"])
 def sendOrder():
-
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
-    channel.queue_declare(queue='hello')
-
-    # msg = session['shop']
-    # msg = msg + "," + request.form.get('name')
-    # msg = msg + "," + request.form.get('tel')
-    # msg = msg + "," + request.form.get('address')
-
-    # for data in session['cart_list']:
-    #     msg = msg + "," + data["id"]
-    #     msg = msg + "," + data["product"]
-    #     msg = msg + "," + str(data["amount"])
-    #     msg = msg + "," + str(data["cost"])
-    
+    # channel.queue_declare(queue='hello') 
     carts = []
     for data in session['cart_list']:
-        cart = {"product_id":data["id"], "name":data["product"], "amount":data["amount"], "cost":data["cost"]}
+        cart = {"product_id":data["id"], "name":data["product"], "amount":data["amount"], "price":data["price"]}
         carts.append(cart)
-
     msg_dict = {
         "Store_name":session['shop']
         , "Cart":carts
         , "Client_information":{"name":request.form.get('name'),"telephone":request.form.get('tel'),"address":request.form.get('address')}
     }
-
     msg_str = json.dumps(msg_dict, indent=3)
-
     channel.basic_publish(exchange='',
                         routing_key='hello',
                         body=msg_str)
-
     connection.close()
-
-    flash(msg_str)
+    session.pop('cart_list')
+    # flash(msg_str)
+    # print(msg_str)
 
     return render_template("sendOrder.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5000)
