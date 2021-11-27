f16abbb101e238ad992fcdd6fa6d12cbebb55077
wichukornk <64311553+wichukornk@users.noreply.github.com>
2021-11-27 19:52:09 +0700

    Release 2.2
    Minor
    - Open app to access from internal ip

--- show_catalog/main.py
+++ show_catalog/main.py
@@ -89,4 +89,5 @@ def sendOrder():
     return render_template("sendOrder.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000) # for development
+    app.run(host="0.0.0.0", port=5000) # for production
